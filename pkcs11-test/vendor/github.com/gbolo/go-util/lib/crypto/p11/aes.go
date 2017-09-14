760c97d82a02691ff84c5d6a4b6108d605cd9950
gbolo <george.bolo@gmail.com>
2017-09-14 17:00:20 -0400

    adding vendoring of packages

---
+++ pkcs11-test/vendor/github.com/gbolo/go-util/lib/crypto/p11/aes.go
@@ -0,0 +1,142 @@
+package p11
+
+import (
+	"os"
+
+	"github.com/miekg/pkcs11"
+	c "github.com/gbolo/go-util/lib/common"
+	"fmt"
+)
+
+/* return a set of attributes that we require for our aes key */
+func GetAesPkcs11Template(objectLabel string, AesKeyLength int, pkcs11LibInfo pkcs11.Info, ephemeral bool) (AesPkcs11Template []*pkcs11.Attribute) {
+
+	// default CKA_KEY_TYPE
+	pkcs11_keytype := pkcs11.NewAttribute(pkcs11.CKA_KEY_TYPE, pkcs11.CKK_AES)
+	HR_keytype := "CKK_AES"
+
+	// Overrides env first, then autodetect from vendor
+	switch {
+	case c.CaseInsensitiveContains(os.Getenv("SECURITY_PROVIDER_CONFIG_KEYTYPE"), "CKK_GENERIC_SECRET"):
+		pkcs11_keytype = pkcs11.NewAttribute(pkcs11.CKA_KEY_TYPE, pkcs11.CKK_GENERIC_SECRET)
+		HR_keytype = "CKK_GENERIC_SECRET"
+	case c.CaseInsensitiveContains(pkcs11LibInfo.ManufacturerID, "softhsm") &&
+		pkcs11LibInfo.LibraryVersion.Major > 1 &&
+		pkcs11LibInfo.LibraryVersion.Minor > 2:
+		// matches softhsm versions greater than 2.2 (scott patched 2.3)
+		pkcs11_keytype = pkcs11.NewAttribute(pkcs11.CKA_KEY_TYPE, pkcs11.CKK_GENERIC_SECRET)
+		HR_keytype = "CKK_GENERIC_SECRET"
+	case c.CaseInsensitiveContains(pkcs11LibInfo.ManufacturerID, "ncipher"):
+		pkcs11_keytype = pkcs11.NewAttribute(pkcs11.CKA_KEY_TYPE, pkcs11.CKK_SHA256_HMAC)
+		HR_keytype = "CKK_SHA256_HMAC"
+	case c.CaseInsensitiveContains(pkcs11LibInfo.ManufacturerID, "SafeNet"):
+		pkcs11_keytype = pkcs11.NewAttribute(pkcs11.CKA_KEY_TYPE, pkcs11.CKK_GENERIC_SECRET)
+		HR_keytype = "CKK_GENERIC_SECRET"
+	}
+
+	// Scott's Reference
+	// default template common to all manufactures
+	AesPkcs11Template = []*pkcs11.Attribute{
+		// common to all
+		pkcs11.NewAttribute(pkcs11.CKA_LABEL, objectLabel),      /* Name of Key */
+		pkcs11.NewAttribute(pkcs11.CKA_TOKEN, !ephemeral),             /* This key should persist */
+		pkcs11.NewAttribute(pkcs11.CKA_VALUE_LEN, AesKeyLength), /* KeyLength */
+		pkcs11.NewAttribute(pkcs11.CKA_SIGN, true),
+		// vendor specific override
+		pkcs11_keytype,
+	}
+
+
+	fmt.Println("PKCS11 Attributes Required:")
+	fmt.Println(" - CKA_KEY_TYPE:", HR_keytype)
+	fmt.Println(" - CKA_LABEL:", objectLabel)
+	fmt.Println(" - CKA_VALUE_LEN:", AesKeyLength)
+	fmt.Println(" - CKA_TOKEN:", !ephemeral)
+	fmt.Println(" - CKA_SIGN:", true)
+
+	return
+}
+
+/* This should verify that our key has the correct attributes */
+func VerifyAesKey(p *pkcs11.Ctx, session pkcs11.SessionHandle, oLabel string, AesKeyLength int, ephemeral bool) (verified bool, err error) {
+
+	// get lib info
+	pkcs11LibInfo, err := p.GetInfo()
+	if err != nil {
+		verified = false
+		return
+	}
+
+	// get the required attributes
+	requiredAttributes := GetAesPkcs11Template(oLabel, AesKeyLength, pkcs11LibInfo, ephemeral)
+
+	// Search for objects which have ALL these attributes
+	oHs, moreThanOne, err := FindObjects(p, session, requiredAttributes, 1)
+
+	// object is verified if there is exactly 1 match and no errors
+	if len(oHs) == 1 && !moreThanOne && err == nil {
+		verified = true
+	}
+
+	return
+}
+
+/* Create an AES key with required template */
+func CreateAesKey(p *pkcs11.Ctx, session pkcs11.SessionHandle, objectLabel string, AesKeyLength int, ephemeral bool) (aesKey pkcs11.ObjectHandle, err error) {
+
+	// get lib info
+	pkcs11LibInfo, err := p.GetInfo()
+	if err != nil {
+		return
+	}
+
+	// default mech CKM_AES_KEY_GEN
+	pkcs11_mech := pkcs11.NewMechanism(pkcs11.CKM_AES_KEY_GEN, nil)
+
+	// Overrides env first, then autodetect from vendor
+	switch {
+	case c.CaseInsensitiveContains(os.Getenv("SECURITY_PROVIDER_CONFIG_MECH"), "CKM_GENERIC_SECRET_KEY_GEN"):
+		pkcs11_mech = pkcs11.NewMechanism(pkcs11.CKM_GENERIC_SECRET_KEY_GEN, nil)
+	case c.CaseInsensitiveContains(pkcs11LibInfo.ManufacturerID, "SafeNet"):
+		pkcs11_mech = pkcs11.NewMechanism(pkcs11.CKM_GENERIC_SECRET_KEY_GEN, nil)
+	}
+
+	// get the required attributes
+	requiredAttributes := GetAesPkcs11Template(objectLabel, AesKeyLength, pkcs11LibInfo, ephemeral)
+
+	// generate the aes key
+	aesKey, err = p.GenerateKey(
+		session,
+		[]*pkcs11.Mechanism{
+			// vendor specific
+			pkcs11_mech,
+		},
+		requiredAttributes,
+	)
+
+	return
+}
+
+/* test CKM_SHA256_HMAC signing */
+func SignHmacSha256(p *pkcs11.Ctx, session pkcs11.SessionHandle, o pkcs11.ObjectHandle, message []byte) (hmac []byte, err error) {
+
+	// start the signing
+	err = p.SignInit(
+		session,
+		[]*pkcs11.Mechanism{
+			pkcs11.NewMechanism(pkcs11.CKM_SHA256_HMAC, nil),
+		},
+		o,
+	)
+	if err != nil {
+		return
+	}
+
+	// do the signing
+	hmac, err = p.Sign(session, message)
+	if err != nil {
+		return
+	}
+
+	return
+}
---
+++ pkcs11-test/vendor/github.com/gbolo/go-util/lib/crypto/p11/ecdsa.go
@@ -0,0 +1,98 @@
+package p11
+
+import (
+	"encoding/asn1"
+	"fmt"
+
+	"github.com/miekg/pkcs11"
+)
+
+/* return a set of attributes that we require for our ecdsa keypair */
+func GetECDSAPkcs11Template(objectLabel string, namedCurve string, ephemeral bool) (pubKeyTemplate []*pkcs11.Attribute, privKeyTemplate []*pkcs11.Attribute, err error) {
+
+	// get ec params
+	ecParam, err := GetECParamMarshaled(namedCurve)
+	if err != nil {
+		return
+	}
+
+	// spec taken from fabric
+	pubKeyTemplate = []*pkcs11.Attribute{
+		pkcs11.NewAttribute(pkcs11.CKA_KEY_TYPE, pkcs11.CKK_EC),
+		pkcs11.NewAttribute(pkcs11.CKA_CLASS, pkcs11.CKO_PUBLIC_KEY),
+		pkcs11.NewAttribute(pkcs11.CKA_TOKEN, !ephemeral), /* session only. destroy later */
+		pkcs11.NewAttribute(pkcs11.CKA_VERIFY, true),
+		pkcs11.NewAttribute(pkcs11.CKA_EC_PARAMS, ecParam),
+		pkcs11.NewAttribute(pkcs11.CKA_PRIVATE, false),
+		pkcs11.NewAttribute(pkcs11.CKA_ID, []byte("PUB-ECDSA-P256")),
+		pkcs11.NewAttribute(pkcs11.CKA_LABEL, "PUB-ECDSA-P256"),
+	}
+
+	// spec taken from fabric
+	privKeyTemplate = []*pkcs11.Attribute{
+		pkcs11.NewAttribute(pkcs11.CKA_KEY_TYPE, pkcs11.CKK_EC),
+		pkcs11.NewAttribute(pkcs11.CKA_CLASS, pkcs11.CKO_PRIVATE_KEY),
+		pkcs11.NewAttribute(pkcs11.CKA_TOKEN, !ephemeral), /* session only. destroy later */
+		pkcs11.NewAttribute(pkcs11.CKA_PRIVATE, true),
+		pkcs11.NewAttribute(pkcs11.CKA_SIGN, true),
+		pkcs11.NewAttribute(pkcs11.CKA_ID, []byte("PRIV-ECDSA-P256")),
+		pkcs11.NewAttribute(pkcs11.CKA_LABEL, "PRIV-ECDSA-P256"),
+		pkcs11.NewAttribute(pkcs11.CKA_EXTRACTABLE, false),
+	}
+
+	fmt.Println("PKCS11 Attributes Required:")
+	fmt.Println(" - CKA_KEY_TYPE:", "CKK_EC")
+	fmt.Println(" - CKA_LABEL:", objectLabel)
+	fmt.Println(" - CKA_EC_PARAMS:", namedCurve)
+	fmt.Println(" - CKA_TOKEN:", !ephemeral)
+	fmt.Println(" - CKA_SIGN:", true)
+
+	return
+}
+
+/* returns value for CKA_EC_PARAMS */
+func GetECParamMarshaled(namedCurve string) (ecParamMarshaled []byte, err error) {
+
+	ecParamOID := asn1.ObjectIdentifier{}
+
+	switch namedCurve {
+	case "P224":
+		ecParamOID = asn1.ObjectIdentifier{1, 3, 132, 0, 33}
+	case "P256":
+		ecParamOID = asn1.ObjectIdentifier{1, 2, 840, 10045, 3, 1, 7}
+	case "P384":
+		ecParamOID = asn1.ObjectIdentifier{1, 3, 132, 0, 34}
+	case "P521":
+		ecParamOID = asn1.ObjectIdentifier{1, 3, 132, 0, 35}
+	}
+
+	if len(ecParamOID) == 0 {
+		err = fmt.Errorf("Error with curve name: %s", namedCurve)
+		return
+	}
+
+	ecParamMarshaled, err = asn1.Marshal(ecParamOID)
+	return
+}
+
+/* Create an ECDSA keypair with required template */
+func CreateECDSAKeyPair(p *pkcs11.Ctx, session pkcs11.SessionHandle, objectLabel string, namedCurve string, ephemeral bool) (ecdsaPrivKey pkcs11.ObjectHandle, ecdsaPubKey pkcs11.ObjectHandle, err error) {
+
+	// get the required attributes
+	pubKeyTemplate, privKeyTemplate, err := GetECDSAPkcs11Template(objectLabel, namedCurve, ephemeral)
+
+	if err != nil {
+		return
+	}
+
+	// generate the ecdsa key
+	ecdsaPubKey, ecdsaPrivKey, err = p.GenerateKeyPair(session,
+		[]*pkcs11.Mechanism{
+			pkcs11.NewMechanism(pkcs11.CKM_EC_KEY_PAIR_GEN, nil),
+		},
+		pubKeyTemplate,
+		privKeyTemplate,
+	)
+
+	return
+}
